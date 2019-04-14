3a5871be5baf78201743fc748879620631fbc4ae
caixw <xnotepad@gmail.com>
2019-04-14 23:22:45 +0800

    改进 next

--- next.go
+++ next.go
@@ -4,26 +4,39 @@
 
 package cron
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // Nexter 用于生成下一次定时器的时间
 type Nexter interface {
 	// 生成下一次定时器需要的时间。
 	// 相对于 last 时间。
 	Next(last time.Time) time.Time
+
+	// Title 生成名称
+	Title() string
 }
 
 // 固定时间段的定时器
 type duration struct {
-	dur time.Duration
+	dur   time.Duration
+	title string
 }
 
 func newDuration(d time.Duration) Nexter {
 	return &duration{
-		dur: d,
+		dur:   d,
+		title: fmt.Sprintf("每隔 %s", d),
 	}
 }
 
 func (d *duration) Next(last time.Time) time.Time {
 	return last.Add(d.dur)
+}
+
+// Title 标题
+func (d *duration) Title() string {
+	return d.title
 }
