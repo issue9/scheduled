29f55b935566a792c9c1011e62d5676f6bf011b0
caixw <xnotepad@gmail.com>
2019-05-03 21:12:31 +0800

    改进测试用例

--- expr_test.go
+++ expr_test.go
@@ -6,7 +6,6 @@ package cron
 
 import (
 	"fmt"
-	"strconv"
 	"testing"
 	"time"
 
@@ -28,60 +27,56 @@ func TestCron_NewExpr(t *testing.T) {
 func TestExpr_Next(t *testing.T) {
 	a := assert.New(t)
 
+	layout := "2006-01-02 15:04:05"
 	type test struct {
 		expr  string
-		times []time.Time // 前一个是后一个的参数
+		times []string // 前一个是后一个的参数
 	}
 
-	base := time.Date(2019, 1, 1, 0, 0, 0, 123, time.UTC) // 周二
-	week := time.Wednesday
-	wdays := week - base.Weekday()
-	if wdays < 0 {
-		wdays += 7
-	}
-	weekdur := time.Duration(wdays) * day
 	var data = []*test{
 		&test{
 			expr: "1 * * * * *",
-			times: []time.Time{
-				base,
-				base.Add(1 * time.Second),
-				base.Add(1 * time.Second).Add(1 * time.Minute), // 进位
-				base.Add(1 * time.Second).Add(2 * time.Minute),
-				base.Add(1 * time.Second).Add(3 * time.Minute),
+			times: []string{
+				"2019-01-01 00:00:00",
+				"2019-01-01 00:00:01",
+				"2019-01-01 00:01:01",
+				"2019-01-01 00:02:01",
+				"2019-01-01 00:03:01",
 			},
 		},
 
 		&test{
 			expr: "* 1 * * * *",
-			times: []time.Time{
-				base,
-				base.Add(1 * time.Minute),
-				base.Add(1 * time.Minute).Add(1 * time.Hour), // 进位
-				base.Add(1 * time.Minute).Add(2 * time.Hour),
-				base.Add(1 * time.Minute).Add(3 * time.Hour),
+			times: []string{
+				"2019-01-01 00:00:00",
+				"2019-01-01 00:01:00",
+				"2019-01-01 01:01:00",
+				"2019-01-01 02:01:00",
+				"2019-01-01 03:01:00",
 			},
 		},
 
 		&test{
 			expr: "1 22 3 * * *",
-			times: []time.Time{
-				base,
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour),
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add(day),
+			times: []string{
+				"2019-01-01 00:00:00",
+				"2019-01-01 03:22:01",
+				"2019-01-02 03:22:01",
+				"2019-01-03 03:22:01",
 			},
 		},
 
 		&test{ // 未指定日，只指定了星期
-			expr: "1 22 3 * * " + strconv.Itoa(int(week)),
-			times: []time.Time{
-				base,
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add(weekdur),
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add(weekdur).Add(7 * day),
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add(weekdur).Add(2 * 7 * day),
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add(weekdur).Add(3 * 7 * day),
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add(weekdur).Add(4 * 7 * day),
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add(weekdur).Add(5 * 7 * day),
+			expr: "1 22 3 * * 3",
+			times: []string{
+				"2019-01-01 00:00:00",
+				"2019-01-02 03:22:01", // 周 3
+				"2019-01-09 03:22:01",
+				"2019-01-16 03:22:01",
+				"2019-01-23 03:22:01",
+				"2019-01-30 03:22:01",
+				"2019-02-06 03:22:01", // 1 月份有 31
+				"2019-02-13 03:22:01",
 			},
 		},
 
@@ -103,32 +98,33 @@ func TestExpr_Next(t *testing.T) {
 		// TODO 2.29 的相关测试
 
 		&test{ // 指定了日和星期
-			expr: "1 22 3 5 * " + strconv.Itoa(int(week)),
-			times: []time.Time{
-				base,
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add(weekdur),                                    // 周 3，2 号
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add(weekdur).Add(3 * day),                       // 周 6，5 号
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add(weekdur).Add((3 + 4) * day),                 // 周 3，9 号
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add(weekdur).Add((3 + 4 + 7) * day),             // 周 3，1.16
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add(weekdur).Add((3 + 4 + 7 + 7) * day),         // 3, 1.23
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add(weekdur).Add((3 + 4 + 7 + 7 + 7) * day),     // 3, 1.30
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add(weekdur).Add((3 + 4 + 7 + 7 + 7 + 6) * day), // 2, 2.6
+			expr: "1 22 3 5 * 3",
+			times: []string{
+				"2019-01-01 00:00:00",
+				"2019-01-02 03:22:01", // 周 3，1.2
+				"2019-01-05 03:22:01", // 周 6，1.5
+				"2019-01-09 03:22:01", // 周 3，1.9
+				"2019-01-16 03:22:01", // 周 3，1.16
+				"2019-01-23 03:22:01", // 周 3, 1.23
+				"2019-01-30 03:22:01", // 周 3, 1.30
+				"2019-02-05 03:22:01", // 周 2, 2.5
+				"2019-02-06 03:22:01", // 周 3, 2.5
 			},
 		},
 
 		&test{
 			expr: "1 22 3 31 * *", // 每个月 31 号
-			times: []time.Time{
-				base,
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add(30 * day),
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add((30 + 28 + 31) * day),                                                             // 3 月
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add((30 + 28 + 31 + 30 + 31) * day),                                                   // 5 月
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add((30 + 28 + 31 + 30 + 31 + 30 + 31) * day),                                         // 7 月
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add((30 + 28 + 31 + 30 + 31 + 30 + 31 + 31) * day),                                    // 8 月
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add((30 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31) * day),                          // 10 月
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add((30 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31) * day),                // 12 月
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add((30 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31 + 31) * day),           // 2020.1 月
-				base.Add(1 * time.Second).Add(22 * time.Minute).Add(3 * time.Hour).Add((30 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + 31 + 31 + 29 + 31) * day), // 2020.3 月
+			times: []string{
+				"2019-01-01 00:00:00",
+				"2019-01-31 03:22:01",
+				"2019-03-31 03:22:01",
+				"2019-05-31 03:22:01",
+				"2019-07-31 03:22:01",
+				"2019-08-31 03:22:01",
+				"2019-10-31 03:22:01",
+				"2019-12-31 03:22:01",
+				"2020-01-31 03:22:01",
+				"2020-03-31 03:22:01",
 			},
 		},
 	}
@@ -142,10 +138,12 @@ func TestExpr_Next(t *testing.T) {
 		a.NotError(err).NotNil(next)
 
 		for j := 1; j < len(t.times); j++ {
-			last := t.times[j-1]
+			last, err := time.Parse(layout, t.times[j-1])
+			a.NotError(err)
 			n := next.Next(last)
 
-			curr := t.times[j]
+			curr, err := time.Parse(layout, t.times[j])
+			a.NotError(err)
 
 			a.Equal(n.Year(), curr.Year(), "year 不同在 %d.times[%d]，返回值：%d，期望值：%d", i, j, n.Year(), curr.Year()).
 				Equal(n.Month(), curr.Month(), "month 不同在 %d.times[%d]，返回值：%d，期望值：%d", i, j, n.Month(), curr.Month()).
