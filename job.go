8e2ac4e46e7d6d41671770bc138f76b073284506
caixw <xnotepad@gmail.com>
2019-05-04 11:04:07 +0800

    实现 job 的相关功能

--- job.go
+++ job.go
@@ -4,7 +4,10 @@
 
 package cron
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // 表示任务状态
 const (
@@ -23,9 +26,11 @@ type JobFunc func() error
 type Job struct {
 	name  string
 	f     JobFunc
-	last  time.Time
+	n     Nexter
 	state State
-	next  Nexter
+	err   error // 出错时的错误内容
+
+	prev, next time.Time
 }
 
 // New 添加一个新的定时任务
@@ -33,7 +38,49 @@ func (c *Cron) New(name string, f JobFunc, n Nexter) {
 	c.jobs = append(c.jobs, &Job{
 		name:  name,
 		f:     f,
-		next:  n,
+		n:     n,
 		state: Stoped,
 	})
+}
+
+// Name 任务的名称
+func (j *Job) Name() string { return j.name }
+
+//
+func (j *Job) Description() string { return j.n.Title() }
+
+// State 获取当前的状态
+func (j *Job) State() State { return j.state }
+
+// 运行当前的任务
+func (j *Job) run() {
+	defer func() {
+		if msg := recover(); msg != nil {
+			if err, ok := msg.(error); ok {
+				j.err = err
+			} else {
+				j.err = fmt.Errorf("job error: %v", msg)
+			}
+
+			j.state = Failed
+		}
+	}()
+
+	j.state = Running
+	j.err = j.f()
+
+	if j.err != nil {
+		j.state = Failed
+	} else {
+		j.state = Stoped
+		j.err = nil
+	}
+
+	j.prev = j.next
+	j.next = j.n.Next(j.next)
+}
+
+// 初始化当前任务，获取其下次执行时间。
+func (j *Job) init(now time.Time) {
+	j.next = j.n.Next(now)
 }
