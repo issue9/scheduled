0444f7420a9ed8edce4a4803cc1b62a16c6152a4
caixw <xnotepad@gmail.com>
2019-05-08 20:26:01 +0800

    合并文件

--- job.go
+++ job.go
@@ -10,6 +10,8 @@ import (
 	"time"
 
 	"github.com/issue9/cron/schedule"
+	"github.com/issue9/cron/schedule/expr"
+	"github.com/issue9/cron/schedule/ticker"
 )
 
 // 表示任务状态
@@ -100,4 +102,44 @@ func (j *Job) run(now time.Time, errlog *log.Logger) {
 // 初始化当前任务，获取其下次执行时间。
 func (j *Job) init(now time.Time) {
 	j.next = j.scheduler.Next(now)
+}
+
+// NewTicker 添加一个新的定时任务
+func (c *Cron) NewTicker(name string, f JobFunc, dur time.Duration) error {
+	return c.New(name, f, ticker.New(dur))
+}
+
+// NewExpr 使用 cron 表示式新建一个定时任务
+//
+// spec 的值可以是：
+//  * * * * * *
+//  | | | | | |
+//  | | | | | --- 星期
+//  | | | | ----- 月
+//  | | | ------- 日
+//  | | --------- 小时
+//  | ----------- 分
+//  ------------- 秒
+//
+// 星期与日若同时存在，则以或的形式组合。
+//
+// 支持以下符号：
+//  - 表示范围
+//  , 表示和
+//
+// 同时支持以下便捷指令：
+//  @yearly:   0 0 0 1 1 *
+//  @annually: 0 0 0 1 1 *
+//  @monthly:  0 0 0 1 * *
+//  @weekly:   0 0 0 * * 0
+//  @daily:    0 0 0 * * *
+//  @midnight: 0 0 0 * * *
+//  @hourly:   0 0 * * * *
+func (c *Cron) NewExpr(name string, f JobFunc, spec string) error {
+	next, err := expr.Parse(spec)
+	if err != nil {
+		return err
+	}
+
+	return c.New(name, f, next)
 }
