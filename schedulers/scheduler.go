f7cc31e1bea979c050436d2f7d881d25c93d256c
caixw <xnotepad@gmail.com>
2019-05-09 22:09:33 +0800

    [schedulers] 接口文档中规定时区信息
    update #3

--- schedulers/scheduler.go
+++ schedulers/scheduler.go
@@ -12,10 +12,12 @@ type Scheduler interface {
 	// 生成下一次的时间。相对于 last 时间。
 	//
 	// 如果不需要再执行了，则应该返回一个零值。
-	//
 	// 如果返回的时间值，已经小于当前时间，那么该任务会被安排在最先执行。
+	//
+	// 实现者应该继承 last 的时区信息，即返回值的时区应该和 last 相同，
+	// 否则其结果是未定义的。
 	Next(last time.Time) time.Time
 
-	// Title 返回当前 Nexter 的一个名称。
+	// Title 返回用于描述当前算法的一个简短介绍。
 	Title() string
 }
