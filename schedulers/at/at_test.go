d6351a12166a7b80d39a802e46800f414842313a
caixw <xnotepad@gmail.com>
2021-10-21 15:19:32 +0800

    test(schedulers/at): at 不再关联时区信息

--- schedulers/at/at_test.go
+++ schedulers/at/at_test.go
@@ -19,18 +19,9 @@ func TestAt(t *testing.T) {
 	// 早于当前时间
 	now := time.Now()
 	tt := now.Add(-time.Hour)
+
 	s := At(tt)
 	a.NotNil(s)
 	a.True(s.Next(now).Before(now)).
 		True(s.Next(now).IsZero()) // 多次获取，返回零值
-
-	const layout = "2006-01-02 15:04:05"
-	ttt, err := time.ParseInLocation(layout, tt.Format(layout), time.UTC) // 擦除时区信息
-	a.NotError(err)
-
-	s = At(tt)
-	a.NotNil(s)
-	loc := time.FixedZone("UTC+8", 8*60*60)
-	next := s.Next(time.Now().In(loc)) // 变成 8 时区，小于零时区的 loc
-	a.True(next.Before(ttt))
 }
